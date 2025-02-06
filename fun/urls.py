985f9d662ae0bcd3ad4a65a594392481ddfa2467
Dee-Abby <124708703+Dee-Abby@users.noreply.github.com>
2025-02-07 00:57:24 +0100

    Update urls.py

--- fun/urls.py
+++ fun/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import classify_number_view
 
 urlpatterns = [
-  path('', classify_number_view, name='classify-numbers')
+  path('classify-number/', classify_number_view, name='classify-numbers')
 ]
